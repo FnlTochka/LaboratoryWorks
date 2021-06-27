f1662edd6c9a254af6df75e825c726e9e6d9c2a6
tochka98 <53086295+tochka98@users.noreply.github.com>
2021-06-27 20:59:44 +0400

    Lab3 и Lab5

--- Lab3/MainWindow.xaml.cs
+++ Lab3/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Lab3
 {
@@ -7,9 +10,134 @@ namespace Lab3
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool checkVar = true;
+
         public MainWindow()
         {
             InitializeComponent();
+            ChangeCheck(true);
+        }
+
+        private void btnClickСalculate(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                switch (checkVar)
+                {
+                    case true:
+                        double _r = Convert.ToDouble(userInputR.Text), _n = Convert.ToDouble(userInputN.Text);
+
+                        double qwe2;
+                        switch (rList.SelectedIndex)
+                        {
+                            case 0:
+                                qwe2 = 1;
+                                break;
+                            case 1:
+                                qwe2 = 0.01;
+                                break;
+                            case 2:
+                                qwe2 = 1;
+                                break;
+                            case 3:
+                                qwe2 = 1000;
+                                break;
+                            case 4:
+                                qwe2 = 0.3048;
+                                break;
+                            case 5:
+                                qwe2 = 0.9144;
+                                break;
+                            case 6:
+                                qwe2 = 0.0254;
+                                break;
+                            case 7:
+                                qwe2 = 1609.344;
+                                break;
+                            default:
+                                qwe2 = 0.001;
+                                break;
+                        }
+
+                        double qwe;
+                        switch (nList.SelectedIndex)
+                        {
+                            case 0:
+                                qwe = Math.PI / 180.0;
+                                break;
+                            case 1:
+                                qwe = 1;
+                                break;
+                            default:
+                                qwe = Math.PI / 180.0;
+                                break;
+                        }
+
+                        double qaaa = qwe * 0.5;
+
+                        double _result = qaaa * qwe2 * Math.Pow(_r, 2.0) * _n;
+                        //double v = _result * 100.0;
+
+                        //result = Math.Pow(_r, 2.0) / 2.0 * (_n - Math.Sin(_n));
+                        userResultStr.Text = Convert.ToString(_result);
+                        break;
+                    case false:
+
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+            catch
+            {
+                userResultStr.Text = Convert.ToString("Некорректный ввод");
+                MessageBox.Show("Поля, соответствующие значению радиуса, углу или длине сектора, должны быть заполнены!");
+            }
+        }
+
+        private void ChangeCheck(bool isEnable)
+        {
+            switch (isEnable)
+            {
+                case true:
+                    checkVar = true;
+                    //canvasCheck.Visibility = Visibility.Visible;
+                    img1.Visibility = Visibility.Visible;
+                    img2.Visibility = Visibility.Hidden;
+                    txt_InputN.Text = "θ:";
+                    userResultStr.Text = Convert.ToString("");
+                    checkArcLength.IsChecked = false;
+                    checkSectorAngle.IsChecked = true;
+                    checkSectorAngle.IsEnabled = false;
+                    checkArcLength.IsEnabled = true;
+                    break;
+                case false:
+                    checkVar = false;
+                    //canvasCheck.Visibility = Visibility.Hidden;
+                    img1.Visibility = Visibility.Hidden;
+                    img2.Visibility = Visibility.Visible;
+                    txt_InputN.Text = "L:";
+                    userResultStr.Text = Convert.ToString("");
+                    checkArcLength.IsChecked = true;
+                    checkSectorAngle.IsChecked = false;
+                    checkSectorAngle.IsEnabled = true;
+                    checkArcLength.IsEnabled = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CheckSectorAngle_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeCheck(true);
+        }
+
+        private void CheckArcLength_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeCheck(false);
         }
     }
 }
--- Lab5/MainWindow.xaml.cs
+++ Lab5/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
 
 namespace Lab5
 {
@@ -14,12 +16,57 @@ namespace Lab5
 
         private void btnClickPaint(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("btnClickPaint");
+            ResultCanvas.Children.Clear();
+
+            double _x = Convert.ToDouble(userInputX.Text), y = 0, R = Convert.ToDouble(userInputR.Text);
+            string function = "";
+            if ((_x < -5) | (_x > 9))
+            {
+                function = "Аргумент за пределами графика!";
+            }
+            else if ((_x < -R) && (_x >= -5))
+            {
+                y = _x + R;
+                function = "y = x + " + Convert.ToString(R); ;
+            }
+            else if ((-R <= _x) && (_x <= 0))
+            {
+                y = Math.Sqrt(Math.Pow(R, 2) - Math.Pow(_x, 2));
+                function = "y = Sqrt( r^2 - x^2 )";
+            }
+            else if ((0 < _x) && (_x < 6))
+            {
+                y = (-0.5 * _x) + 3;
+                function = "y = (-0.5 * x) + 3";
+            }
+            else if ((6 <= _x) && (_x < 9))
+            {
+                y = _x - 6;
+                function = "y = x - 6";
+            }
+            userResultY.Text = Convert.ToString(y);
+
+            userResultStr.Text = function;
+
+            // тестовая линия
+            Line myLine;
+            myLine = new Line
+            {
+                Stroke = System.Windows.Media.Brushes.Black,
+                X1 = 1,
+                X2 = 50,
+                Y1 = 1,
+                Y2 = 50,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                StrokeThickness = 2
+            };
+            ResultCanvas.Children.Add(myLine);
         }
 
         private void btnClickClear(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("btnClickClear");
+            ResultCanvas.Children.Clear();
         }
     }
 }
